afabfdb6905624be5aab76cc5ec0bde2f886aea3
Edward_Loza <lozaedward@gmail.com>
2021-08-08 17:47:13 -0700

    Add components, props and prototypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
+
     return (
-      <div className="App">
-        <h1>Hello from react</h1>
+      <div className='App'>
+        <Navbar />
       </div>
     );
   }
