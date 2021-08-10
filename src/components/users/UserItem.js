b668e9a0d08770c887304157e8bdd4b179efd913
Edward_Loza <lozaedward@gmail.com>
2021-08-09 23:23:30 -0700

    Fix URLs in UserItem.js

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -4,7 +4,7 @@ class UserItem extends Component {
     state = {
         id: 'id',
         login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
         html_url: 'https://github.com/mojombo'
     };
 
@@ -15,7 +15,7 @@ class UserItem extends Component {
                 <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }}></img>
                 <h3>{login}</h3>
                 <div>
-                    <a href="{html_url}" className="btn btn-dark btn-sm my-1">
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">
                         More
                     </a>
 
